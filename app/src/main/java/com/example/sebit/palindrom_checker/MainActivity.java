10210855d79e2935beb85925f2a776eb523f98c3
SebastianAT <setr780@gmail.com>
2018-03-07 18:04:10 +0100

    Last Update - str Reserve Method
    error if field is empty
    error if field is <5
    only lowercase allowed

--- app/src/main/java/com/example/sebit/palindrom_checker/MainActivity.java
+++ app/src/main/java/com/example/sebit/palindrom_checker/MainActivity.java
@@ -19,29 +19,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public static boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
     public void Berechnung(View v) {
 
-        EditText textInputField = (EditText) findViewById(R.id.Text1);   // Obejkt Variable erstellen mit R.Id auf layout zugreifen und zahl01 holen also verweiß
-        TextView textOutputField = (TextView) (findViewById(R.id.textView2));
+        EditText textInputField = (EditText) findViewById(R.id.Text1);   // Objekt Variable erstellen mit R.Id auf layout zugreifen und Text1 holen also verweiß
+        TextView textOutputField = (TextView) (findViewById(R.id.textView2));  // auch mit id textView2
 
-        String palindrom = textInputField.getText().toString();
+        String palindrom = textInputField.getText().toString();   // holt Value vom Input Textfeld als String
 
-        if(palindrom.isEmpty()){
+        if(palindrom.isEmpty()){                              // prüft ob Input Textfeld leer ist -> Fehlermeldung
             textOutputField.setText("Field is empty");
-        } else if(palindrom.length() == 1){
-            textOutputField.setText("Only one char");
+        } else if(palindrom.length() < 5){                  // prüft ob weniger als 5 Zeichen sind -> Fehlermeldung
+            textOutputField.setText("At least 5 chars");
         } else {
 
+            if(isPalindrome(palindrom) == true){           // hier wird dann nochmal geprüft ob True / false für Palindrom -> Meldung
+                textOutputField.setText(String.valueOf(palindrom + " is a Palindrom"));
+            } else {
+                textOutputField.setText(String.valueOf(palindrom + " is not a Palindrom"));
+            }
+
+
+
+            /*
+            * First Version
+            *
             int lng = palindrom.length();
 
 
-            for (int i = 0; i < lng / 2; i++) {
-                if (palindrom.charAt(i) == palindrom.charAt(lng - i - 1)) {
-                    textOutputField.setText(String.valueOf(palindrom + " is a Palindrom"));
+
+            for (int i = 0; i < lng/2; i++) {
+                if (palindrom.charAt(i) != palindrom.charAt(lng-i-1)) {
+                   textOutputField.setText(String.valueOf(palindrom + " is not a Palindrom"));
+
                 } else {
-                    textOutputField.setText(String.valueOf(palindrom + " is not Palindrom"));
+                    textOutputField.setText(String.valueOf(palindrom + " is a Palindrom"));
                 }
             }
+            */
+
         }
 
 
